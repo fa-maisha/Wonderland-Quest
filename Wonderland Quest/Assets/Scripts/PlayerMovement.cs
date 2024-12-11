5a6a75c05c86922d135e0d15f2f64e82e8636fd4
mhsakibb <mhssaakkiibb@gmail.com>
2024-12-11 13:13:28 +0600

    Scene Changes

---
+++ Wonderland Quest/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] public float speed;
+    public Rigidbody2D body;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+        if (horizontalInput > 0.01f)
+        {
+            transform.localScale = Vector3.one;
+        }
+        else if(horizontalInput < -0.01f)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        }
+
+    }
+}
